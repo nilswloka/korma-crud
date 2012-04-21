6f1dc7a708da4088b923d7aa2f98a716d4993731
Norbert Schneider <Norbert-Schneider@gmx.de>
2012-04-21 10:35:21 +0200

    Added Clojure and lein-swank dependencies.

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject korma-crud "0.0.1-SNAPSHOT"
   :description "Create CRUD views for Korma-queries"
-  :dependencies [[korma "0.3.0-beta7"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [korma "0.3.0-beta7"]
                  [postgresql "8.4-702.jdbc4"]
                  [compojure "1.0.3"]
                  [ring "1.1.0"]
                  [hiccup "1.0.0"]]
-  :plugins [[lein-cucumber "1.0.0"]]
+  :plugins [[lein-cucumber "1.0.0"]
+            [lein-swank "1.4.2"]]
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
   :profiles {:dev {:dependencies [[midje "1.4.0-beta1"]]}})
